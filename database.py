bcd3725d9137a427391f934c753b327976ce44dd
hilarychizi <hilarychizi@gmail.com>
2023-08-08 17:51:18 +0100

    Store application in db

--- database.py
+++ database.py
@@ -34,3 +34,16 @@ def load_job_from_db(id):
             return None
         else:
             return dict(rows[0])
+
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query=text("insert into applications (job_id, full_name, email, linkedin, work_experience, resume_url, education) values(:job_id, :full_name, :email, :linkedin, :work_experience, :resume_url, :education)")
+        conn.execute(query,job_id=job_id, 
+                     full_name=data['full_name'],
+                     linkedin=data['linkedin'],
+                     email=data['email'],
+                     work_experience=data['work_experience'],
+                     resume_url=data['resume_url'],
+                     education=data['education']
+                     )
+
